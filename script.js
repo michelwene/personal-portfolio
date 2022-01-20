2f5331b5288a9ed8c27905b396f4b3f8bb818fb6
Michel Wene <michelwene@hotmail.com>
2022-01-20 17:04:58 -0300

    finish project mode light

---
+++ script.js
@@ -0,0 +1,19 @@
+var slider = tns({
+  container: ".my-slider",
+  items: 1,
+  speed: 600,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  controlsContainer: "#custom-control",
+  mode: "gallery",
+  responsive: {
+    0: {
+      nav: true,
+      navPosition: "bottom",
+      autoplay: false,
+    },
+    1024: {
+      autoplay: true,
+    },
+  },
+});
